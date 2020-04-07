25b0015ede3ec92f16640fc4da50bb70bb4d7b5b
Pavel Filipenco <filipencop@mail.ru>
2020-04-07 19:57:10 +0300

    modified colors

--- config.h
+++ config.h
@@ -16,11 +16,13 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_border[]      = "#1E1E1E";
+static const char col_orange[]      = "#cf6a4c";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_border },
+	[SchemeSel]  = { col_gray4, col_orange, col_orange },
 };
 
 /* tagging */
@@ -32,9 +34,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       2,            0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "discord",  NULL,       NULL,       3,            0,           -1 },
-	{ "Gimp",     NULL,       NULL,       4,            1,           -1 },
+	{ "Gimp",     NULL,       NULL,       4,            0,           -1 },
 };
 
 /* layout(s) */
