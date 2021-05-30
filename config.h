045ce183993842de71c0ec69fb37c1d79cf26077
Filipenco Pavel <filipencop@mail.ru>
2021-05-30 17:11:05 +0300

    Firefox picture in picture now floats.

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[] = {
     "xos4 Terminus:size=9",
 };
-static const char norm_bg[]      = "#282828";
-static const char sel_bg[]       = "#1D2021";
+static const char norm_bg[]      = "#1D2021";
+static const char sel_bg[]       = "#282828";
 static const char norm_border[]  = "#282828";
 
 static const char norm_fg[]      = "#a89984";
@@ -45,13 +45,14 @@ static const Rule rules[] = {
      *      WM_CLASS(STRING) = instance, class
      *      WM_NAME(STRING) = title
      */
-    /* class                instance        title   tags mask       isfloating      monitor */
-    { "firefox",            NULL,           NULL,   1 << 3,         0,              -1 },
-    { "discord",            NULL,           NULL,   1 << 7,         0,              -1 },
-    { "UnityHub",           NULL,           NULL,   1 << 5,         1,              -1 },
-    { "Unity",              NULL,           NULL,   1 << 5,         0,              -1 },
-    { "Lingot",             NULL,           NULL,   0,              1,              -1 },
-    { "TelegramDesktop",    NULL,           NULL,   1 << 7,         0,              -1 },
+    /* class                instance        title                   tags mask       isfloating      monitor */
+    { "firefox",            NULL,           NULL,                   1 << 3,         0,              -1 },
+    { "firefox",            NULL,           "Picture-in-Picture",   1 << 3,         1,              -1 },
+    { "discord",            NULL,           NULL,                   1 << 7,         0,              -1 },
+    { "UnityHub",           NULL,           NULL,                   1 << 5,         1,              -1 },
+    { "Unity",              NULL,           NULL,                   1 << 5,         0,              -1 },
+    { "Lingot",             NULL,           NULL,                   0,              1,              -1 },
+    { "TelegramDesktop",    NULL,           NULL,                   1 << 7,         0,              -1 },
 };
 
 /* layout(s) */
